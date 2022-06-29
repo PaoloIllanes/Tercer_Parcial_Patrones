98dc4e75c8c0b9daacb4b364881d7a6e662df8fc
Paolo <oeuthfoierughi@gmail.com>
2022-06-28 23:52:02 -0400

    Initial commit

---
+++ src/main/java/adapter/strcuture/ElementConcreteB.java
@@ -0,0 +1,13 @@
+package adapter.strcuture;
+
+public class ElementConcreteB implements  IAdapter{
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    public void method2(String attr1, String attr2) {
+
+    }
+}
---
+++ src/main/java/adapter/strcuture/IAdapter.java
@@ -0,0 +1,6 @@
+package adapter.strcuture;
+
+public interface IAdapter {
+    void method1();
+    void method2(String attr1,String attr2);
+}
